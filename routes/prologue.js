60da5f78966a400bbd95af60bc06cbea36c5c01c
Jihoon (JJ) Jung <Nnoitra5+GitHub@gmail.com>
2023-03-28 23:19:34 -0600

    Minor update to prologue template code.

--- routes/prologue.js
+++ routes/prologue.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+// Set up the Prologue block
 router.get('/', (req, res) => {
-  res.send('This is the prologue block.');
+  const playerName = req.query.playerName;
+  if (!playerName) {
+    return res.status(400).send('Error: player name not provided.');
+  }
+  res.send(`Welcome, ${playerName}! This is the Prologue block.`);
 });
 
 module.exports = router;
